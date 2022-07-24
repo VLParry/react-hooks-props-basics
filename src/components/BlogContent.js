e73487d72c6405a2dafbac30f75f5b3cc231c7e8
Victoria Parry <vlparry@gmail.com>
2022-07-23 18:02:44 -0600

    commit tree ac6aca72d270729f7770219993b1cdc56e7e2b00

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
-}
+  console.log(props)
+  if(!props.isPublished){
+    return null;
+  } else {
+    
+  
+  return ( 
+  // <div id="blog-content">{props.articleText}</div>;
+ <div>
+  <h1>{props.articleText}</h1>
+  <p>{props.miuntesToRead} </p>
+  </div>
+  );
+}}
 
 export default BlogContent;
